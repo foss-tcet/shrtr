9f4543f54fdc06b2d50a2f5b908151330f3b47cc
Kartik Verma <vkartik97+github@gmail.com>
2018-01-07 03:01:19 +0530

    Added Redirect for short urls

--- server.js
+++ server.js
@@ -2,12 +2,37 @@ const express = require('express')
 
 const app = express()
 
-// Homepage
+const db = require('./configs/db')
+
 app.use("/", express.static(__dirname + "/static"));
 
 app.use('/includes', express.static(__dirname + '/includes'));
 
+function concatHTTP(url) {
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = "http://" + url;
+    }
+    return url;
+}
+
+app.get('/favicon.ico', function(req, res) {
+    res.status(204);
+})
+
+app.get('/:url', (req, res, next) => {
+    const db_query = "Select sh_url, url from shrtr_db_main where sh_url = " + db.escape(req.params.url)
+    db.query(db_query, (error, results, fields) => {
+        if (error){
+            res.send(error)
+            throw error
+        }
+        res.redirect(concatHTTP(results[0].url))
+    })
+    
+})
+
 app.use((req, res, next) => {
+    console.log("Throwing 404 Error for " +req.url)
     res.status('404')
     res.send("404 Error!")
 })
